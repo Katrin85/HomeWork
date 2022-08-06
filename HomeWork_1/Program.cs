51962c21fec794a036bcd365d8aba1d04e4f5890
Katrin85 <Em-1985@yandex.ru>
2022-08-06 19:36:44 +0300

    Task №1 - addition

--- HomeWork_1/Program.cs
+++ HomeWork_1/Program.cs
@@ -1,10 +1,16 @@
-﻿//Задача №1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿
+//Задача №1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 Console.WriteLine ("Введите первое число");
 int numA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите второе число");
 int numB = Convert.ToInt32(Console.ReadLine());
 int max = numA;
+int min = numA;
 if (numA > max) max = numA;
 if (numB > max) max = numB;
+if (numA < min) min = numA;
+if (numB < min) min = numB;
+
 Console.WriteLine ("Максимальное число = " + max);
+Console.WriteLine ("Минимальное число = " + min);
