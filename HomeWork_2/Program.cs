3c45aeac0221fa9939d93a37999efb7d5ced6dad
Katrin85 <Em-1985@yandex.ru>
2022-08-11 20:33:07 +0300

    task №3

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -12,7 +12,7 @@ int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
 int newNumber = InputThreeDigitNumber(ThreeDigitNumber);
 Console.WriteLine(newNumber);
 */
-
+/*
 // Задача №2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 int InputNumber (int number)
@@ -21,12 +21,10 @@ int InputNumber (int number)
     {
         number = number / 10;
     } 
-    if (number <= 99)
+    if (number < 100)
     {
-        Console.WriteLine($"В числе {number} нет третьей цифры"); 
-        
+        Console.WriteLine($"В числе {number} нет третьей цифры");
     } 
-
     else 
     {
        number = number % 10;
@@ -38,3 +36,17 @@ Console.Write("Введите число: ");
 int Number = Convert.ToInt32(Console.ReadLine());
 int newNumber = InputNumber(Number);
 Console.WriteLine($"Третье цифра числа {Number} - {newNumber}");
+*/
+// Задача №3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void DayWeek (int day) 
+{
+    if (day == 6 || day == 7)
+       Console.WriteLine("Это выходной день");
+       
+    if (day >= 1 && day <= 5)
+       Console.WriteLine("Это рабочий день, подожди немного и будет выходной.");
+}
+Console.WriteLine("Введиите цифру, обозначающую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+DayWeek(day);
