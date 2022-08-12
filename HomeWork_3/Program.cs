c65a2acb70211fba080b1fa38eca61af0cb1aec3
Katrin85 <Em-1985@yandex.ru>
2022-08-12 21:53:10 +0300

    HomeWork_3

--- HomeWork_3/Program.cs
+++ HomeWork_3/Program.cs
@@ -1,19 +1,1 @@
 ﻿
-
-
-static void SecondMethod(int inputUserNum)
-        {
-            Console.WriteLine("Решение 2-м способом");
-            int a = inputUserNum;
-            if (a < 100)
-                Console.WriteLine($"     В числе {a} нет третьей цифры");
-            else
-            {
-                while (a>=1000)
-                {
-                    a = a / 10;
-                }    
-                a = a % 10;
-                Console.WriteLine("     Третья цифра числа " + inputUserNum + " является " + a);
-            }
-        }
