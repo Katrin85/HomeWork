03fa1834f0be94bf9b1d13c888d310033e5792fa
Katrin85 <Em-1985@yandex.ru>
2022-08-17 12:24:04 +0300

    Task №1

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -1,7 +1,20 @@
 ﻿// Задача №1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+ double DegreeNumber (double x, double y)
+            {
 
+                double a = Math.Pow(x, y);
+                return a;
+
+            }
+
+            Console.WriteLine("Введите число: ");
+            double digit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите в какую степень будем возводить: ");
+            double number = Convert.ToInt32(Console.ReadLine());
+            double resultat = DegreeNumber(digit, number);
+            Console.WriteLine($"Ответ: {resultat}");
 // Задача №2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
