74ddf82f2db327a19fe0413d78d55389f943be9e
Katrin85 <Em-1985@yandex.ru>
2022-08-17 15:56:09 +0300

    Task №2

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -1,10 +1,12 @@
 ﻿// Задача №1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
- double DegreeNumber (double x, double y)
+// Вариант №1.
+/*
+ double DegreeNumber (double x, double y)  // int x - число; int y- степень
             {
 
-                double a = Math.Pow(x, y);
+                double a = Math.Pow(x, y);   // возводим в степень при помощи формулы
                 return a;
 
             }
@@ -15,10 +17,54 @@
             double number = Convert.ToInt32(Console.ReadLine());
             double resultat = DegreeNumber(digit, number);
             Console.WriteLine($"Ответ: {resultat}");
+*/
+// Вариант №2ю
+/*
+int DegreeNumber(int x, int y) 
+            {
+
+                int result = 1;
+                for (int i = 1; i <= y; i++) // при помощи цикла for возводим число в степень.
+                {
+                    result = result * x;
+                }
+                return result;
+            }
+
+            Console.WriteLine("Введите число: ");
+            int digit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите в какую степень будем возводить: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int resultat = DegreeNumber(digit, number);
+            Console.WriteLine($"Ответ: {resultat}");
+*/
 // Задача №2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+/*
+int Count(int number)
+            {
+                int size = 0;
+                int score = 0;
+                int counter = Convert.ToString(number).Length;
+
+                for (int i = 0; i < counter; i++)
+                {
+                    score = number - number % 10;
+                    size = size + (number - score);
+                    number = number / 10;
+                }
+                return size;
+            }
+
+            Console.WriteLine("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = Count(num);
+            Console.WriteLine("Сумма цифр в числе = " +sum);
+    */
+
+
 
 // Задача №3. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
